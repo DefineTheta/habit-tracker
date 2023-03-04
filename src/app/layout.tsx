54f3cc891ca90372cd1aad4a43a5da6ed4cb1dbc
Parthey Bhatt <>
2023-03-04 00:50:07 -0800

    Setup Next.js and tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className='flex justify-center items-center h-full w-full'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
